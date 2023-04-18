7bf3628fdaf43c47dc26308553a683b9824a93a1
Stevie Militello <94778206+steviemilitello@users.noreply.github.com>
2023-04-18 00:03:02 -0400

    adds staircase

---
+++ 007-staircase.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function staircase(n) {
+    // Write your code here
+    // set base cases
+    for (let i = 1; i <= n; i++) {   
+        // rows are equal to n 
+        // print, right aligned
+        // prints out n-1 times and append a # i times
+        console.log(" ".repeat(n-i) + "#".repeat(i))
+    }
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    staircase(n);
+}
