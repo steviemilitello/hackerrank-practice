63a8ffa1f1b6dcaf119f744e8931b5f2fd200ad8
Stevie Militello <94778206+steviemilitello@users.noreply.github.com>
2023-04-26 20:39:10 -0400

    adds between two sets

---
+++ 014-between-two-sets.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function getTotalX(a, b) {
+    // Write your code here
+    // find common multiples in first array a
+    // find common factors (divisors) of the second array b
+    // set count for return value
+    let count = 0 
+    for (let i = 1; i <= 100; i++) {
+        // use every to check if all elements in array pass a test 
+        // for every value in in the array, is i evenly divisible by it 
+        if (a.every(int => (i % int ==0))) {
+            // same with b, if they both equal 0, add to the count
+            if (b.every(int => (int % i == 0)))
+            // add to count
+            count++
+        }
+    }
+    return count
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const brr = readLine().replace(/\s+$/g, '').split(' ').map(brrTemp => parseInt(brrTemp, 10));
+
+    const total = getTotalX(arr, brr);
+
+    ws.write(total + '\n');
+
+    ws.end();
+}
