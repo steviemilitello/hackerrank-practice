c7a27f7f66e594195a19a8f9e21a0dad0392b4fa
Stevie Militello <94778206+steviemilitello@users.noreply.github.com>
2023-05-04 01:46:07 -0400

    adds sales by match

---
+++ 021-sales-by-match.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, ar) {
+    // Write your code here
+    // set variable for count
+    let count = 0
+    // store number in new array
+    const newAr = {}
+        // loop through array using for each
+        ar.forEach(i => {
+            // if the number has occured before, add to count, which means it is a pair
+            if(newAr[i]) {
+                count += 1
+                newAr[i] = 0
+            // if not, the entry of that in the object is 1
+            } else {
+                newAr[i] = 1
+            }
+        })
+        return count
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = sockMerchant(n, ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
